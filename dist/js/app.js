bca433100d7f5335f9b50c56126f7d13043ba989
Dima_Lobodenko <60902427+Sontenor@users.noreply.github.com>
2023-09-22 16:40:18 +0100

    Add files via upload

--- dist/js/app.js
+++ dist/js/app.js
@@ -59,6 +59,25 @@
             }
         }));
     }
+    function formQuantity() {
+        document.addEventListener("click", (function(e) {
+            let targetElement = e.target;
+            if (targetElement.closest("[data-quantity-plus]") || targetElement.closest("[data-quantity-minus]")) {
+                const valueElement = targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]");
+                let value = parseInt(valueElement.value);
+                if (targetElement.hasAttribute("data-quantity-plus")) {
+                    value++;
+                    if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) value = valueElement.dataset.quantityMax;
+                } else {
+                    --value;
+                    if (+valueElement.dataset.quantityMin) {
+                        if (+valueElement.dataset.quantityMin > value) value = valueElement.dataset.quantityMin;
+                    } else if (value < 1) value = 1;
+                }
+                targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]").value = value;
+            }
+        }));
+    }
     let addWindowScrollEvent = false;
     function headerScroll() {
         addWindowScrollEvent = true;
@@ -97,5 +116,6 @@
     window["FLS"] = true;
     isWebp();
     menuInit();
+    formQuantity();
     headerScroll();
 })();
